e12273a6c09411f949ac876426fcf88de074a0b9
huangjinzhou <h.j.z@outlook.com>
2018-09-04 15:41:20 +0800

    add simple id lock

---
+++ src/main/java/site/hjz/util/locks/SimpleIdLock.java
@@ -0,0 +1,31 @@
+package site.hjz.util.locks;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * @param <T>
+ * @author huangjinzhou
+ * @date 2018年9月4日14点53分
+ */
+public class SimpleIdLock<T> {
+    private static final long DEFAULT_LOCK_WAIT_TIME = 100;
+
+    private static final Object FLAG = new Object();
+
+    public SimpleIdLock(int capacity) {
+        this.cache = new ConcurrentHashMap<>(capacity);
+    }
+
+    private final ConcurrentMap<T, Object> cache;
+
+    public void lock(T id) throws InterruptedException {
+        while (cache.putIfAbsent(id, FLAG) != null) {
+            Thread.sleep(DEFAULT_LOCK_WAIT_TIME);
+        }
+    }
+
+    public void unlock(T id) {
+        cache.remove(id);
+    }
+}
---
+++ src/test/java/site/hjz/util/locks/SimpleIdLockTest.java
@@ -0,0 +1,75 @@
+package site.hjz.util.locks;
+
+import org.junit.Test;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class SimpleIdLockTest {
+
+    @Test
+    public void test() {
+        final SimpleIdLock<String> stringIdLock = new SimpleIdLock<>(4);
+        final long start = System.currentTimeMillis();
+        final CyclicBarrier barrier = new CyclicBarrier(3, new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("time spend : " + (System.currentTimeMillis() - start));
+            }
+        });
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    stringIdLock.lock("a");
+                    System.out.println("Thread1 begin");
+                    for (int i = 0; i < 10; i++) {
+                        System.out.println("Thread 1 running");
+                        Thread.sleep(100);
+                    }
+                    System.out.println("Thread1 end");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                stringIdLock.unlock("a");
+                try {
+                    barrier.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    stringIdLock.lock("b");
+                    System.out.println("Thread2 begin");
+                    for (int i = 0; i < 10; i++) {
+                        System.out.println("Thread 2 running");
+                        Thread.sleep(100);
+                    }
+                    System.out.println("Thread2 end");
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                stringIdLock.unlock("b");
+                try {
+                    barrier.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        try {
+            barrier.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+    }
+}
